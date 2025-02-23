f92ee1a9c6afc61a90a4c7eae58b9a16b3275d39
vladislav602 <martynnazar11@gmail.com>
2025-02-23 14:12:40 +0200

    gvbedbsgvf

--- JOB.py
+++ JOB.py
@@ -2,8 +2,8 @@ import requests
 
 url = "https://api.coingecko.com/api/v3/simple/price"
 params = {
-    "ids": "bitcoin",
-    "vs_currencies": "usd,uah"
+    "ids": "ethereum",
+    "vs_currencies": "uah"
 }
 
 response = requests.get(url, params=params)
--- main.py
+++ main.py
@@ -11,31 +11,52 @@ app.setStyleSheet("""
         QWidget {
             background: linear-gradient(135deg, #282c34, #3e4451);
             color: #8be9fd;
-            font-family: sans-serif;
+            font-family: fantasy;
         }
         
         QPushButton
         {
             background-color:  #3e4451;
             border-style: solid;
-            font-family: sans-serif;
+            font-family: fantasy;
             padding: 10px 20px;
             font-size: 16px;
             color: #8be9fd;
             border-width: 2px ;
             border-color: #61afef;
             border-radius:5px;
-            box-shadow: 0 0 10px #61afef, 0 0 20px #8be9fd;
-            transition: background-color 0.3s ease, box-shadow 0.3s ease;
+            
             
         }
-        
-        
-        QPushButton:hover 
-        {
-             background-color: #52596a;
-             box-shadow: 0 0 15px #8be9fd, 0 0 25px #a4ffff;
-    
+         
+       QLineEdit
+       {    
+            background-color: #898f8f;
+            border-style: outset;
+            font-family: fantasy;
+            padding:  20px;
+            font-size: 16px;
+            color: #000000;
+            border-width: 2px ;
+            border-color: #22e9ccf;
+            border-radius:5px;
+            
+       }
+            
+       QComboBox
+       {
+            background-color: #898f8f;
+            border-style: outset;
+            font-family: fantasy;
+            padding:  20px;
+            font-size: 16px;
+            color: #000000;
+            border-width: 2px ;
+            border-color: #22e9ccf;
+            border-radius:5px;
+            
+            
+       }
 """)
 
 
@@ -57,8 +78,50 @@ vidpovid = QPushButton("Конвертація")
 
 
 
+combo = QComboBox()
+combo.addItem("Bitcoin")
+combo.addItem("Litecoin")
+combo.addItem("Namecointcoin")
+combo.addItem("SwiftCoin")
+combo.addItem("BlackCoin")
+combo.addItem("Dash")
+combo.addItem("Ethereum")
+combo.addItem("SixEleven")
+combo.addItem("Titcoin")
+combo.addItem("Synereo AMP")
+combo.addItem("Nxt")
+combo.addItem("Monero")
+combo.addItem("DigitalNote")
+combo.addItem("Burstcoin")
+combo.addItem("Ripple")
+
+
+
+combo2 = QComboBox()
+combo2.addItem("AUD")
+combo2.addItem("CZK")
+combo2.addItem("CNY")
+combo2.addItem("INR")
+combo2.addItem("JPY")
+combo2.addItem("KZT")
+combo2.addItem("UAH")
+combo2.addItem("USD")
+combo2.addItem("EUR")
+combo2.addItem("PLN")
+combo2.addItem("BGN")
+combo2.addItem("XDR")
+combo2.addItem("GBR")
+combo2.addItem("GEL")
+combo2.addItem("SAR")
+
+
+dodatkova_line = QHBoxLayout()
+dodatkova_line.addWidget(combo)
+dodatkova_line.addWidget(combo2)
+
 
 main_line = QVBoxLayout()
+main_line.addLayout(dodatkova_line)
 main_line.addWidget(val1_input)
 main_line.addWidget(val2_input)
 main_line.addWidget(cilckist_input)
@@ -76,7 +139,8 @@ def get_result():
 
     response = requests.get(url, params=params)
     data = response.json()
-    print(data[val1_input.text()][val2_input.text()])
+    bit = print(data[val1_input.text()][val2_input.text()])
+    result_input.setText(bit*cilckist_input)
 
 vidpovid.clicked.connect(get_result)
 
